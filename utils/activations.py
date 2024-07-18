4c6fe2cbbbcfe6161276473e314c91ead6a3219e
a-mhamdi <a_mhamdi@outlook.com>
2024-07-18 15:13:23 +0100

    activation functions updated

--- utils/activations.py
+++ utils/activations.py
@@ -1,18 +1,23 @@
 import numpy as np
 
+
 ### TANH: x -> (e^x - e^-x) / (e^x + e^-x)
 def tanh(x):
     return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 
+
 def tanh_prime(x):
     return 1 - tanh(x) ** 2
+
 
 ### SIGMOID: x -> 1 / (1 + e^-x)
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+
 def sigmoid_prime(x):
     return x * (1 - x)
+
 
 ### SOFTMAX: x -> exp(x) / sum(exp(x))
 def softmax(x):
@@ -20,26 +25,33 @@ def softmax(x):
     sum_exps = sum(exps)
     return [i / sum_exps for i in exps]
 
+
 def softmax_prime(x):
     pass
+
 
 ### LINEAR: x -> x
 def linear(x):
     return x
 
+
 def linear_prime(x):
-    return 1
+    return np.ones(x.shape)
+
 
 ### RELU: x -> max(0, x)
 def relu(x):
-    return x if x > 0 else 0
+    return np.maximum(x, 0)
+
 
 def relu_prime(x):
-    return 1 if x > 0 else 0
+    return np.where(x >= 0., 1, 0)
+
 
 ### LEAKY RELU: x -> max(alpha * x, x)
 def leaky_relu(x, alpha=0.01):
-    return x if x > 0 else alpha * x
+    return np.maximum(alpha * x, x)
+
 
 def leaky_relu_prime(x, alpha=0.01):
-    return 1 if x > 0 else alpha
+    return np.where(x >= 0, 1, alpha)
