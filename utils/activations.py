b5b73af397a6b74d1b950a75d190a190c0e506a7
a-mhamdi <a_mhamdi@outlook.com>
2024-08-11 19:13:37 +0100

    update activation functions

--- utils/activations.py
+++ utils/activations.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-### TANH: x -> (e^x - e^-x) / (e^x + e^-x)
+# TANH: x -> (e^x - e^-x) / (e^x + e^-x)
 def tanh(x, diff=False):
     if diff:
         return 1 - tanh(x) ** 2
@@ -9,7 +9,7 @@ def tanh(x, diff=False):
         return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 
 
-### SIGMOID: x -> 1 / (1 + e^-x)
+# SIGMOID: x -> 1 / (1 + e^-x)
 def sigmoid(x, diff=False):
     if diff:
         return sigmoid(x) * (1 - sigmoid(x))
@@ -17,17 +17,17 @@ def sigmoid(x, diff=False):
         return 1 / (1 + np.exp(-x))
 
 
-### SOFTMAX: x -> exp(x) / sum(exp(x))
+# SOFTMAX: x -> exp(x) / sum(exp(x))
 def softmax(x, diff=False):
     if diff:
-        return softmax(x)/(1-softmax(x))
+        pass
     else:
         exps = [np.exp(i) for i in x]
         sum_exps = sum(exps)
         return [i / sum_exps for i in exps]
 
 
-### LINEAR: x -> x
+# LINEAR: x -> x
 def linear(x, diff=False):
     if diff:
         return np.ones(x.shape)
@@ -35,7 +35,7 @@ def linear(x, diff=False):
         return x
 
 
-### RELU: x -> max(0, x)
+# RELU: x -> max(0, x)
 def relu(x, diff=False):
     if diff:
         return np.where(x >= 0, 1, 0)
@@ -43,10 +43,9 @@ def relu(x, diff=False):
         return np.maximum(x, 0)
 
 
-### LEAKY RELU: x -> max(alpha * x, x)
+# LEAKY RELU: x -> max(alpha * x, x)
 def leaky_relu(x, alpha=0.01, diff=False):
     if diff:
         return np.where(x >= 0, 1, alpha)
     else:
         return np.maximum(alpha * x, x)
-
