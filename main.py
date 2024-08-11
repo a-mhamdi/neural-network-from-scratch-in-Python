e0c67c8f350b821b8cda45de2dbea8bc18c9e12f
a-mhamdi <a_mhamdi@outlook.com>
2024-08-11 19:51:42 +0100

    simulation setup

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
+from sklearn import datasets
+
 import numpy as np
 from matplotlib import pyplot as plt
 
 from utils.preprocessing import train_test_split
-from utils.activations import relu
-from utils.mlp import MLP, loss_fct
+from utils.mlp import MLP
+from utils.activations import relu, sigmoid
+from utils.metrics import loss_fct, accuracy
 
+np.random.seed(1234)
 
-## Synthetic data
-n_samples, n_features, epochs = 1024, 3, 8
-X = .7 * np.random.rand(n_samples, n_features) + 2.
-y = -1.5 * np.sum(X, axis=1) + 5.
+# Dataset Load
+ds = datasets.load_breast_cancer()
+X, y = ds.data, ds.target
+
+n_features, epochs = X.shape[1], 16
+
+try:
+    n_targets = y.shape[1]
+except IndexError:
+    n_targets = 1
+
 (X_train, X_test), (y_train, y_test) = train_test_split(X, y)
-model = MLP([[n_features, 4, relu], [4, 7, relu], [7, 1, relu]])
+
+# Standardization/ Normalization
+mu, std = np.mean(X_train, axis=0), np.std(X_train, axis=0)
+# X_train, X_test = (X_train - mu) / std, (X_test - mu) / std
+
+mn, mx = np.min(X_train, axis=0), np.max(X_train, axis=0)
+X_train, X_test = (X_train - mn) / (mx - mn), (X_test - mn) / (mx - mn)
+
+# Model Design
+model = MLP([[n_features, 16, relu], [8, relu], [4, relu], [n_targets, sigmoid]])
 model.summary()
-settings = {'loss': 'rmse', 'optimizer': 'sgd', 'lr': 0.01, 'regularizer': 'None', 'lambda': 0.01, 'r': 0.5, 'dropout': 0.}
+
+settings = {
+    'batch_size': 12,
+    'loss': 'bce',
+    'optimizer': 'sgd',
+    'lr': 1e-2,
+    'regularization': 'none',
+    'lambda': 0.1,
+    'r': 0.5
+}
 
 ltrn, ltst = [], []
 for epoch in range(epochs):
@@ -23,8 +52,21 @@ for epoch in range(epochs):
     ytst_hat = model.predict(X_test)  # predicted `test` output
     ltst.append(loss_fct(y_test, ytst_hat, hparams=settings))
 
-plt.plot(ltrn, label='train')
-plt.plot(ltst, label='test')
-plt.legend()
-plt.show()
+    print(f"{accuracy(y_train, ytrn_hat): .3f}", f"{accuracy(y_test, ytst_hat): .3f}", sep=' | ')
 
+fig, ax = plt.subplot_mosaic([['a', 'b'], ['a', 'c']], layout='constrained')
+
+ax['a'].set_title('loss')
+ax['a'].plot(ltrn, label='train')
+ax['a'].plot(ltst, label='test')
+ax['a'].legend()
+
+ax['b'].set_title('actual vs. predicted (train)')
+ax['b'].scatter(range(len(y_train)), y_train)
+ax['b'].scatter(range(len(ytrn_hat)), (ytrn_hat >= .5) * 1)
+
+ax['c'].set_title('actual vs. predicted (test)')
+ax['c'].scatter(range(len(y_test)), y_test)
+ax['c'].scatter(range(len(ytst_hat)), (ytst_hat >= .5) * 1)
+
+plt.show()
--- utils/mlp.py
+++ utils/mlp.py
@@ -1,22 +1,8 @@
 import numpy as np
 
 from utils.preprocessing import data_loader
-from utils import activations
-
-default_hparams = {'loss': 'mse', 'optimizer': 'sgd', 'lr': 0.01, 'regularizer': 'None', 'lambda': 0.01, 'r': 0.5, 'dropout': 0.}
-
-
-def loss_fct(y, yhat, hparams=default_hparams):
-    if hparams['loss'].lower() == 'mae':
-        loss = np.mean(np.abs(y - yhat))
-    elif hparams['loss'].lower() == 'mse':
-        loss = np.mean((y - yhat) ** 2)
-    elif hparams['loss'].lower() == 'rmse':
-        loss = np.sqrt(np.mean((y - yhat) ** 2))
-    elif hparams['loss'].lower() == 'cross-entropy':
-        loss = -np.mean(y * np.log(yhat) + (1 - y) * np.log(1 - yhat))
-
-    return loss
+from utils.activations import relu, leaky_relu, sigmoid, softmax
+from utils.metrics import loss_fct
 
 
 class MLP():
@@ -27,68 +13,109 @@ class MLP():
         self.act = [None] * leng
         self.leng = leng
         for l in range(leng):
-            self.W[l] = np.random.randn(arch[l][0], arch[l][1])
-            self.b[l] = np.random.rand(1, arch[l][1])
-            self.act[l] = arch[l][2]
+            if l == 0:
+                self.act[l] = arch[l][2]
+                size_out, size_in = arch[l][0], arch[l][1]
+                self.b[l] = np.zeros(arch[l][1])
+            else:
+                self.act[l] = arch[l][1]
+                size_out, size_in = self.b[l - 1].shape[0], arch[l][0]
+                self.b[l] = np.zeros(arch[l][0])
+
+            if self.act[l] == sigmoid or self.act[l] == softmax:
+                # `Glorot/Xavier` Normal
+                limit = np.sqrt(2 / float(size_out + size_in))
+                self.W[l] = np.random.normal(0., limit, size=(size_in, size_out))
+            elif self.act[l] == relu or self.act[l] == leaky_relu:
+                # `He` Normal
+                limit = np.sqrt(2 / float(size_out))
+                self.W[l] = np.random.normal(0., limit, size=(size_in, size_out))
+            else:
+                # LeCun Normal
+                limit = np.sqrt(1 / float(size_out))
+                self.W[l] = np.random.normal(0., limit, size=(size_in, size_out))
 
     def __len__(self):
         return self.leng
 
     def summary(self):
-        print('Model Summary: ', '==============', sep='\n')
-        print(36*'-')
+        tot_params = 0
+        print(70*'=', 30*' ' + 'Model Summary', sep='\n')
+        print(70*'-')
         for l in range(len(self)):
-            print('| Layer #{} => '.format(l), end='')
-            print('W' + str(l) + ': ' + str(self.W[l].shape), end=' ')
-            print('and b' + str(l) + ': ' + str(self.b[l].shape[1]) + ' |')
-            print(36*'-')
+            msg = '| Layer #' + str(l) + ' => ' \
+                  + 'W' + str(l) + ': ' + str(self.W[l].shape) \
+                  + ' and b' + str(l) + ': ' + str(self.b[l].shape[0])
 
-    def __feedforward(self, X, hparams=default_hparams):
-        A, H = [], [X]
+            params = np.prod(self.W[l].shape) + self.b[l].shape[0]
+            tot_params += params
+            str_params = str(params) + ' trainable parameters'
+            print(msg + (40-len(msg))*' ' + ' | ' + str_params + (25-len(str_params))*' ' + ' |')
+            print(70*'-')
+        print('Total trainable parameters: ' + str(tot_params), 70*'=', sep='\n')
+
+    def __feedforward(self, x):
+        z, a = [], [x.squeeze()]
         for l in range(len(self)):
-            A.append(H[-1] @ self.W[l] + self.b[l])
-            H.append(self.act[l](A[-1]))
-        return A, H  # pre-activation, post-activation (i.e., hypothesis at each layer)
+            z.append((self.W[l] @ a[-1]) + self.b[l])
+            a.append(self.act[l](z[-1]))
 
-    def __backprop(self, y, A, H, hparams=default_hparams):
-        loss, dW, db = 0., [], []
+        return z, a  # pre-activation, post-activation (i.e., at each layer: a = f(z), where z = W @ a + b)
 
-        delta = [np.array((y-H[-1]) * self.act[-1](A[-1], diff=True)).squeeze()]
-        dW.append(-H[-2].T * delta[-1])
-        db.append(-delta[-1])
+    def __backprop(self, z, a, y, hparams):
+        dW, db = [], []
+        loss, dlt = loss_fct(y, a[-1], hparams, is_dlt=True)
+        delta = [np.array(dlt * self.act[-1](z[-1], diff=True))]
+        for l in range(len(self)-2, -1, -1):
+            delta.append((delta[-1] @ self.W[l+1]) * self.act[l](z[l], diff=True))
+        delta = delta[::-1]
 
-        for l in range(len(self)-1, -1, -1):
-            delta.append(self.act[l](A[l], diff=True) * np.dot(self.W[l+1].T, delta[-1]))
-            dW.insert(0, -H[-l-2].T * delta[-1])
-            db.insert(0, -delta[-1])
-
-        loss = loss_fct(y, H[-1], hparams=hparams)
         for l in range(len(self)):
-            if hparams['regularizer'].lower() == 'l1':
+            dW.append(-np.reshape(delta[l], (-1, 1)) * np.reshape(a[l], (1, -1)))
+            db.append(-delta[l])
+
+        if hparams['regularization'].lower() == 'l1':
+            for l in range(len(self)):
                 loss += hparams['lambda'] * np.sum(np.abs(self.W[l]))
                 dW[l] += hparams['lambda'] * np.sign(self.W[l])
-            elif hparams['regularizer'].lower() == 'l2':
+        elif hparams['regularization'].lower() == 'l2':
+            for l in range(len(self)):
                 loss += 0.5 * hparams['lambda'] * np.sum(self.W[l] ** 2)
                 dW[l] += hparams['lambda'] * self.W[l]
-            elif hparams['regularizer'].lower() == 'elasticnet':
-                loss += hparams['lambda'] * hparams['r'] * np.sum(np.abs(self.W[l])) + hparams['lambda'] * (1-hparams['r']) * np.sum(self.W[l] ** 2)
-                dW[l] += hparams['lambda'] * hparams['r'] * np.sign(self.W[l]) + hparams['lambda'] * (1-hparams['r']) * self.W[l]
+        elif hparams['regularization'].lower() == 'elasticnet':
+            for l in range(len(self)):
+                loss += hparams['lambda'] * (hparams['r'] * np.sum(np.abs(self.W[l])) + (1-hparams['r']) * np.sum(self.W[l] ** 2))
+                dW[l] += hparams['lambda'] * (hparams['r'] * np.sign(self.W[l]) + (1-hparams['r']) * self.W[l])
 
         return loss, dW, db
 
-    def fit(self, X, y, hparams=default_hparams):
-        for (data_X, data_y) in data_loader(X, y):
-            ## Forward pass
-            A, H = self.__feedforward(data_X, hparams=hparams)
-            ## Backward pass
-            loss, dW, db = self.__backprop(data_y, A, H, hparams=hparams)
+    def fit(self, x, y, hparams):
+        batch_size = hparams['batch_size']
+        for (data_x, data_y) in data_loader(x, y, batch_size=batch_size):
+            loss, dW, db = 0., [np.zeros(w.shape) for w in self.W], [np.zeros(b.shape) for b in self.b]
+            for i in range(data_x.shape[0]):
+                ## Forward pass
+                z, a = self.__feedforward(data_x[i])
+                ## Backward pass
+                l, gradW, gradb = self.__backprop(z, a, data_y[i], hparams=hparams)
+                loss += l
+                for l in range(len(self)):
+                    dW[l] += gradW[l]
+                    db[l] += gradb[l]
+
+            loss /= batch_size
             print('loss >>> train {}'.format(loss))
             ## Update weights
             for l in range(len(self)):
-                self.W[l] -= hparams['lr'] * dW[l]
-                self.b[l] -= hparams['lr'] * db[l]
+                self.W[l] -= hparams['lr'] * dW[l] / batch_size
+                self.b[l] -= hparams['lr'] * db[l] / batch_size
 
-    def predict(self, X):
-        A, H = self.__feedforward(X)
+    def predict(self, x):
+        leng, n_targets = x.shape[0], self.b[-1].shape[0]
+        out = np.zeros((leng, n_targets))
+        for i in range(leng):
+            data = x[i, :]
+            _, a = self.__feedforward(data)
+            out[i, :] = a[-1]
 
-        return H[-1]
+        return out
